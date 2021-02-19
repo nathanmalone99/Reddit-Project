b1799cb3400dfbd6ee939845f964ab17f14e4a26
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2021-02-19 16:36:10 +0000

    Made the container for submitting url's and titles, also linked bootstrap to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reddit-project';
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean {
+    console.log('Adding Article Title : and adding Article link ')
+    return false
+  }
 }
